321deb6253b6a4e3022ff91a4cdc655589b926aa
Viktor <victorpatrin@gmail.com>
2023-08-23 16:28:37 +0700

    menu

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,12 +5,14 @@ import styles from '../style/logo.module.scss';
 const Logo: FunctionComponent = () => {
   return (
     <div className={styles.container}>
-      <h1 className='largeHeader marginBottom1em'>
-        Методическое объединение
-      </h1>
-      <h2 className='mediumHeader'>
-        МКОУ Сокурская СОШ №19
-      </h2>
+      <div className={styles.text}>
+        <h1 className='largeHeader marginBottom1em'>
+          Методическое объединение
+        </h1>
+        <h2 className='mediumHeader'>
+          МКОУ Сокурская СОШ №19
+        </h2>
+      </div>
     </div>
   );
 };
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,18 @@
 import { FunctionComponent } from 'react';
 
+import styles from '../style/menu.module.scss';
+import stylesBtn from '../style/ui/buttons.module.scss';
+
+import { menu } from '../data/menu';
+
+import Button from './ui/Button';
+
 const Menu: FunctionComponent = () => {
   return (
-    <div style={{backgroundColor: 'red', width: '100%', height: '500px'}}>
+    <div className={styles.container}>
+      {
+        menu.map((item, index) => <Button btnText={item} key={index} className={stylesBtn.menuBtn} />)
+      }
     </div>
   );
 };
